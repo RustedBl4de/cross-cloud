f7c0fcb08b19827bfba7edf91f977da020f8361a
RustedBl4de <rusty.chapin+github@gmail.com>
2022-11-21 21:23:44 -0800

    Adding swagger

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,19 @@
 package main
 
+// @title User API documentation
+// @version 1.0.0
+// @host localhost:3000
+// @BasePath /user
+
 import (
 	"github.com/RustedBl4de/cross-cloud/pkg/common/db"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
+
+	_ "github.com/RustedBl4de/cross-cloud/cmd/docs"
+
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 func main() {
@@ -15,6 +25,8 @@ func main() {
 
 	r := gin.Default()
 	db.Init(dbUrl)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
