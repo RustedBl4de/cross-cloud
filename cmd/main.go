a42b6a0ae31d8c7ae6b943499bdf131603468e84
RustedBl4de <rusty.chapin+github@gmail.com>
2022-11-21 21:43:00 -0800

    Adding user functions

--- cmd/main.go
+++ cmd/main.go
@@ -12,8 +12,45 @@ import (
 
 	_ "github.com/RustedBl4de/cross-cloud/cmd/docs"
 
+	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// GetUsers ... Get all users
+// @Summary Get all users
+// @Description get all users
+// @Tags Users
+// @Success 200 {array} model.User
+// @Failure 404 {object} object
+// @Router / [get]
+func GetUser(c *gin.Context) {
+	return
+}
+
+// CreateUser ... Create User
+// @Summary Create new user based on paramters
+// @Description Create new user
+// @Tags Users
+// @Accept json
+// @Param user body model.User true "User Data"
+// @Success 200 {object} object
+// @Failure 400,500 {object} object
+// @Router / [post]
+func CreateUser(c *gin.Context) {
+	return
+}
+
+// GetUserByID ... Get the user by id
+// @Summary Get one user
+// @Description get user by ID
+// @Tags Users
+// @Param id path string true "User ID"
+// @Success 200 {object} model.User
+// @Failure 400,404 {object} object
+// @Router /{id} [get]
+func GetUserByID(c *gin.Context) {
+	return
+}
 
 func main() {
 	viper.SetConfigFile("./pkg/common/envs/.env")
@@ -25,14 +62,14 @@ func main() {
 	r := gin.Default()
 	db.Init(dbUrl)
 
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(ginSwagger.swaggerFiles.Handler))
-
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"port":  port,
 			"dbUrl": dbUrl,
 		})
 	})
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	r.Run(port)
 }
